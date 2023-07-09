faf2bde1bbdf06a027f3b3183ac2072bc385e6d3
siddhant1 <sidhant.manchanda@gmail.com>
2023-07-09 17:05:34 +0530

    Added long polling

---
+++ long-polling/server.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"time"
+)
+
+type Job struct {
+	Progress int16
+	ID       uuid.UUID
+}
+
+type SuccessResposnse struct {
+	JobID uuid.UUID
+}
+
+type JobRequest struct {
+	ID string `param:"jobId"`
+}
+
+var jobs []*Job
+
+func NewJob() *Job {
+	return &Job{
+		Progress: 0,
+		ID:       uuid.New(),
+	}
+}
+
+func main() {
+	e := echo.New()
+
+	e.POST("/api/setJob", func(c echo.Context) error {
+
+		newJob := NewJob()
+		jobs = append(jobs, newJob)
+		UpdateJobProgress(newJob)
+
+		return c.JSON(http.StatusOK, SuccessResposnse{JobID: newJob.ID})
+	})
+
+	e.GET("/api/getJob/:jobId", func(c echo.Context) error {
+
+		jobReq := JobRequest{
+			ID: c.Param("jobId"),
+		}
+		var job *Job
+		for _, element := range jobs {
+			if element.ID.String() == jobReq.ID {
+				job = element
+			}
+		}
+
+		for {
+			if isJobComplete(job) {
+				break
+			}
+		}
+
+		return c.JSON(http.StatusOK, job)
+
+	})
+
+	e.Logger.Fatal(e.Start(":8000"))
+}
+
+func isJobComplete(job *Job) bool {
+	if job.Progress == 100 {
+		return true
+	}
+
+	return false
+}
+
+func UpdateJobProgress(job *Job) {
+	if job.Progress == 100 {
+		return
+	}
+	time.AfterFunc(time.Duration(int64(time.Second)), func() {
+		job.Progress += 5
+		UpdateJobProgress(job)
+	})
+}
